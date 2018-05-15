0a6a6062530aa7e4068e014c59faef4d0dc598c2
maria.abas <maria.abas@est.fib.upc.edu>
2018-05-15 23:21:52 +0100

    Primera provar de creació de la url mitjançant txt

--- repository.py
+++ repository.py
@@ -1,15 +1,24 @@
-import requests
+import urllib2
 
 def obtenir_assignatures():
-	url_assig="https://raco.fib.upc.edu/api/horaris/assignatures-titulacio.txt
-	req_assig=requests.get(url_assig)
+	url_assig="https://raco.fib.upc.edu/api/horaris/assignatures-titulacio.txt?codi=GRAU"
+	req_assig=urllib2.Request
+	response_assig = urllib2.urlopen(url_assig)
 	file_assig=open("assignatures_grau.txt", "w")
-	file_assig.write(res_assig.read())
+	file_assig.write(response_assig.read())
 	file_assig.close()
-	return file_assig
+
 
 def obtenir_informacio_assignatures():
 	url_info_assig="https://raco.fib.upc.edu/api/horaris/horari-assignatures.txt?"
-	file_assig=obtenir_assignatures()
-	for assig in file_assig.readline()
-		print(assig)
+	obtenir_assignatures()
+	file_assig=open("assignatures_grau.txt", "r")
+	file_aux=open("horari_totes_assignatures", "w")
+	suma="assignatures=" + file_assig.readline().rstrip('\n')
+	for assig in file_assig:
+		suma = suma	+ "&assignatures=" + assig.rstrip('\n')
+	
+	print(suma)
+	#file_aux.write(suma)
+	file_assig.close()
+	file_aux.close()
